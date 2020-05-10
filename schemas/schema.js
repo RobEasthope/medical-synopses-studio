7d46adc2a7dae09141f1d8d3f500adc302efd993
Rob Easthope <hello@robeasthope.com>
2020-05-10 17:17:39 +0100

    Import synopsis schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // Document types
+import synopsis from './documents/synopsis'
 import category from './documents/category'
 
 // Objects
@@ -21,6 +22,7 @@ export default createSchema({
     simplePortableText,
     
     // Documents
+    synopsis,
     category
   ])
 })
