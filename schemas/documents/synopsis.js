2b3ac5792c9904812e4c2bc078bbad4bce620721
Rob Easthope <hello@robeasthope.com>
2020-05-10 17:32:15 +0100

    Add category and add group fields

--- schemas/documents/synopsis.js
+++ schemas/documents/synopsis.js
@@ -4,50 +4,67 @@ export default {
     title: 'Synopsis',
     fields: [
         {
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-            validation: Rule => Rule.required()
+            name: 'Paper',
+            type: 'object',
+            fields: [
+                {
+                    name: 'title',
+                    title: 'Title',
+                    type: 'string',
+                    validation: Rule => Rule.required()
+                },
+                {
+                    name: 'slug',
+                    title: 'Slug',
+                    type: 'slug',
+                    description: 'Some frontends will require a slug to be set to show the project',
+                    options: {
+                        source: 'title',
+                        maxLength: 96
+                    },
+                    validation: Rule => Rule.required()
+                },
+                {
+                    name: 'paperUrl',
+                    title: 'Paper URL',
+                    type: 'url',
+                },
+                {
+                    name: 'paperPublicationDate',
+                    title: 'Paper publication datte',
+                    type: 'date',
+                },
+                {
+                    name: 'category',
+                    title: 'Category',
+                    type: 'reference',
+                    to: [{ type: 'category' }]
+                },
+            ]
         },
         {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            description: 'Some frontends will require a slug to be set to show the project',
-            options: {
-                source: 'title',
-                maxLength: 96
-            },
-            validation: Rule => Rule.required()
+            name: 'Synopsis',
+            type: 'object',
+            fields: [
+                {
+                    name: 'methods',
+                    title: 'Methods',
+                    type: 'simplePortableText',
+                    validation: Rule => Rule.required()
+                },
+                {
+                    name: 'keyFindings',
+                    title: 'Key findings',
+                    type: 'simplePortableText',
+                    validation: Rule => Rule.required()
+                },
+                {
+                    name: 'implications',
+                    title: 'Implications',
+                    type: 'simplePortableText',
+                    validation: Rule => Rule.required()
+                }
+            ]
         },
-        {
-            name: 'paperUrl',
-            title: 'paperUrl',
-            type: 'url',
-        },
-        {
-            name: 'paperPublicationDate',
-            title: 'Paper publication datte',
-            type: 'date',
-        },
-        
-        {
-            name: 'methods',
-            title: 'Methods',
-            type: 'simplePortableText',
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'keyFindings',
-            title: 'Key findings',
-            type: 'simplePortableText',
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'implications',
-            title: 'Implications',
-            type: 'simplePortableText',
-            validation: Rule => Rule.required()
-        }
     ]
 }
