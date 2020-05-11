b6952cb94c5987ffa2fba6f20893724eca2bf467
Rob Easthope <hello@robeasthope.com>
2020-05-11 17:09:09 +0100

    First pass at new structure

---
+++ deskStructure.js
@@ -0,0 +1,44 @@
+import S from '@sanity/desk-tool/structure-builder';
+import {
+  GoBook,
+  GoBookmark,
+  GoBriefcase,
+  GoCode,
+  GoNote,
+  GoPaintcan,
+  GoPerson,
+  GoRocket,
+  GoTerminal,
+} from 'react-icons/go';
+import { FaRegAddressCard, FaUniversity } from 'react-icons/fa';
+
+// const hiddenTypes = ['about'];
+
+export default () =>
+  S.list()
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Synopses')
+        .schemaType('synopsis')
+        .icon(GoPaintcan)
+        .child(S.documentTypeList('synopsis')),
+      S.listItem()
+        .title('Pages')
+        .icon(GoBook)
+        .child(
+          S.list()
+            .title('Pages')
+            .items([
+              S.listItem()
+                .title('About page')
+                .child(
+                  S.editor()
+                    .id('about')
+                    .schemaType('about')
+                    .documentId('about')
+                )
+                .icon(GoNote),
+            ])
+        ),
+    ]);
