727e42ac6344b084a6e4272ec7836e2b0ac747c3
Rob Easthope <hello@robeasthope.com>
2020-05-11 15:41:39 +0100

    Upgrade rich text block

--- schemas/objects/simplePortableText.js
+++ schemas/objects/simplePortableText.js
@@ -6,24 +6,6 @@ export default {
     {
       title: 'Block',
       type: 'block',
-      // Styles let you set what your user can mark up blocks with. These
-      // corrensponds with HTML tags, but you can set any title or value
-      // you want and decide how you want to deal with it where you want to
-      // use your content.
-      styles: [{ title: 'Normal', value: 'normal' }],
-      lists: [],
-      // Marks let you mark up inline text in the block editor.
-      marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting by editors.
-        decorators: [
-          { title: 'Strong', value: 'strong' },
-          { title: 'Emphasis', value: 'em' },
-          { title: 'Code', value: 'code' },
-        ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
-        annotations: [],
-      },
     },
   ],
 };
