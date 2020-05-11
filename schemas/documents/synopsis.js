99515406f5653f5f51f0cd091ca3db123df9e9f3
Rob Easthope <hello@robeasthope.com>
2020-05-11 15:06:37 +0100

    Fix slug generation and add fieldsets

--- schemas/documents/synopsis.js
+++ schemas/documents/synopsis.js
@@ -2,74 +2,76 @@ export default {
   name: 'synopsis',
   type: 'document',
   title: 'Synopsis',
+  fieldsets: [
+    { name: 'paperDetails', title: 'Paper details' },
+    { name: 'synopsis', title: 'Synopsis' },
+  ],
   fields: [
     {
-      name: 'paper',
-      title: 'Paper details',
-      type: 'object',
-      fields: [
-        {
-          name: 'title',
-          title: 'Title',
-          type: 'string',
-          validation: Rule => Rule.required(),
-        },
-        {
-          name: 'slug',
-          title: 'Slug',
-          type: 'slug',
-          description:
-            'Some frontends will require a slug to be set to show the project',
-          options: {
-            source: 'title',
-            maxLength: 96,
-          },
-          validation: Rule => Rule.required(),
-        },
-        {
-          name: 'paperUrl',
-          title: 'Paper URL',
-          type: 'url',
-          validation: Rule => Rule.required(),
-        },
-        {
-          name: 'paperPublicationDate',
-          title: 'Paper publication date',
-          type: 'date',
-        },
-        {
-          name: 'category',
-          title: 'Category',
-          type: 'reference',
-          to: [{ type: 'category' }],
-          validation: Rule => Rule.required(),
-        },
-      ],
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      fieldset: 'paperDetails',
+      validation: Rule => Rule.required(),
     },
     {
-      name: 'synopsis',
-      title: 'Synopsis',
-      type: 'object',
-      fields: [
-        {
-          name: 'methods',
-          title: 'Methods',
-          type: 'simplePortableText',
-          validation: Rule => Rule.required(),
-        },
-        {
-          name: 'keyFindings',
-          title: 'Key findings',
-          type: 'simplePortableText',
-          validation: Rule => Rule.required(),
-        },
-        {
-          name: 'implications',
-          title: 'Implications',
-          type: 'simplePortableText',
-          validation: Rule => Rule.required(),
-        },
-      ],
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      fieldset: 'paperDetails',
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: input =>
+          input
+            .toLowerCase()
+            .replace(/\s+/g, '-')
+            .slice(0, 200),
+        validation: Rule => Rule.required(),
+      },
+    },
+    {
+      name: 'paperUrl',
+      title: 'Paper URL',
+      type: 'url',
+      fieldset: 'paperDetails',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'paperPublicationDate',
+      title: 'Paper publication date',
+      type: 'date',
+      fieldset: 'paperDetails',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{ type: 'category' }],
+      fieldset: 'paperDetails',
+      validation: Rule => Rule.required(),
+    },
+
+    {
+      name: 'methods',
+      title: 'Methods',
+      type: 'simplePortableText',
+      fieldset: 'synopsis',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'keyFindings',
+      title: 'Key findings',
+      type: 'simplePortableText',
+      fieldset: 'synopsis',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'implications',
+      title: 'Implications',
+      type: 'simplePortableText',
+      fieldset: 'synopsis',
+      validation: Rule => Rule.required(),
     },
   ],
   preview: {
