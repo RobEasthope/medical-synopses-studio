8903291a07757b9befc1f72ee0aca52bb66f678f
Rob Easthope <hello@robeasthope.com>
2020-05-16 21:14:07 +0100

    Append lang code to end of slug if title is set in right language

--- schemas/locale/localeSlug.js
+++ schemas/locale/localeSlug.js
@@ -16,7 +16,12 @@ export default {
     type: 'slug',
     fieldset: lang.isDefault ? null : 'translations',
     options: {
-      source: doc => `${doc.title[`${lang.id}`]}`,
+      source: doc => {
+        if (doc.title[`${lang.id}`]) {
+          return `${doc.title[`${lang.id}`]}-${lang.id}`;
+        }
+        return null;
+      },
       maxLength: 200,
       validation: Rule => Rule.required(),
     },
