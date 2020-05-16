6ca406e3d85e91336b2f6e085da6895516a2c52d
Rob Easthope <hello@robeasthope.com>
2020-05-16 15:04:31 +0100

    Copy in localeString example

---
+++ schemas/locale/localeString.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'localeString',
+  type: 'object',
+  fieldsets: [
+    {
+      title: 'Translations',
+      name: 'translations',
+      options: { collapsible: true },
+    },
+  ],
+  fields: supportedLanguages.map(lang => ({
+    title: lang.title,
+    name: lang.id,
+    type: 'string',
+    fieldset: lang.isDefault ? null : 'translations',
+  })),
+};
